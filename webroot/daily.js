25d5945975c707ded6c6ceaec7a0e1055827fd8d
Angelicpancake <145518259+Angelicpancake@users.noreply.github.com>
2025-03-22 17:57:50 -0400

    daily class

--- webroot/daily.js
+++ webroot/daily.js
@@ -1,1 +1,40 @@
-document
+/*
+    backend of the daily words grabber
+*/
+
+/*
+    buttons
+*/
+this.skipBtn =  /** @type {HTMLButtonElement} */ (
+    document.querySelector('#skipBtn')
+);
+
+/*
+    event listeners
+*/
+this.skipBtn.addEventListener('click', ()=> {
+    test();
+});
+
+let dailyWords = new Array(3);
+
+class Daily { //define word
+    constructor(english, word) {
+        this.english = english;
+        this.word = word;
+    }
+
+    getWord() {
+        return this.word;
+    }
+
+    getEnglish() {
+        return this.english;
+    }
+}
+
+function test() {
+    dailyWords[0] = new Daily("Food", "食べ物");
+    console.log(dailyWords[0].getWord());
+}
+
